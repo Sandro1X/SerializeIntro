c40046b6533e6aaa4173b013005555c714e144ee
Sandro Baumgartner <bausac16@htl-kaindorf.ac.at>
2018-09-28 12:08:53 +0200

    save tester

--- SerializeIntro/src/SchuelerBL.java
+++ SerializeIntro/src/SchuelerBL.java
@@ -1,13 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author baumg
- */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+
 public class SchuelerBL {
+    private ArrayList<Schueler> schueler = new ArrayList<>();
     
+    public void add (Schueler s){
+        schueler.add(s);
+    }
+    
+    public void save(File f)throws Exception{
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        
+        for (Schueler s : schueler) {
+            bw.write(s.getName());
+            bw.write(";");
+            bw.write(s.getBirthday().toString());
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+    
+    public void load()throws Exception{
+        
+    }
 }
